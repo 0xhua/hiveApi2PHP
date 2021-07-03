37d8378c4bb35b8e6ad64d7a99945bf05306a2ae
0xhua <60700937+0xhua@users.noreply.github.com>
2021-07-04 01:24:48 +0800

    Update Lib

--- lib/hiveApi2.php
+++ lib/hiveApi2.php
@@ -1,6 +1,7 @@
 <?php
 require_once './vendor/stefangabos/zebra_curl/Zebra_cURL.php';
 
+
 class hiveApi2
 {
     private $baseURL = 'https://api2.hiveos.farm/api/v2/';
@@ -22,7 +23,8 @@ class hiveApi2
         $this->tfa = $tfa;
     }
 
-    private function login()
+
+    private function login(bool $remember = false)
     {
         $curl = new Zebra_cURL();
         $curl->ssl(false);
@@ -34,22 +36,75 @@ class hiveApi2
                 $this->baseURL.'auth/login' => json_encode(array(
                         'login' => $this->login,
                         'password' => $this->password,
-                        'twofa_code'=> $this->tfa
+                        'twofa_code'=> $this->tfa,
+                        'remember' => $remember
                     )
                 )), function ($result) {
-                var_dump($result);
             if ($result->response[1] == CURLE_OK) {
                 if ($result->info['http_code'] == 200) {
-                        var_dump(json_decode($result->body, true));
+                    /**
+                     * STATUS 200
+                     {
+                       "access_token": "string",
+                       "token_type": "bearer",
+                       "expires_in": 0
+                      }*/
+                    @$this->result=json_decode($result->body);
                 }elseif ($result->info['http_code'] == 403){
-
+                    /**
+                     * STATUS 403
+                    	Not allowed for authenticated users
+                    */
+                    @$this->result=json_decode($result->body, true);
+                }elseif ($result->info['http_code'] == 422){
+                    /**
+                     * STATUS 422
+                    	{
+                          "message": "The given data was invalid.",
+                          "errors": {
+                            "field1": [
+                              "The field1 is required"
+                            ],
+                            "field2": [
+                              "The field2 must be a string",
+                              "The field2 must be 6 characters length"
+                            ]
+                          }
+                        }
+                    */
+                    @$this->result=json_decode($result->body, true);
                 } else trigger_error('Server responded with code ' . $result->info['http_code'], E_USER_ERROR);
             } else trigger_error('cURL responded with: ' . $result->response[0], E_USER_ERROR);
         }
         );
+        return $this->result;
     }
 
-    public function start(){
-        @$this->login();
+    private function logout($token){
+        $curl = new Zebra_cURL();
+        $curl->ssl(false);
+        $curl->option(CURLOPT_HTTPHEADER,[
+            "Authorization: Bearer $token"
+        ]);
+        $curl->post($this->baseURL.'auth/logout',function ($result) {
+            var_dump($result);
+            if ($result->response[1] == CURLE_OK) {
+                if ($result->info['http_code'] == 401) {
+                    /**
+                     * STATUS 401
+                    {
+                    "message": "Unauthenticated."
+                    }*/
+                    @$this->result=json_decode($result->body, true);
+                }elseif ($result->info['http_code'] == 204){
+                    /**
+                     * STATUS 204
+                    Successful logout
+                     */
+                    @$this->result=json_decode($result->body, true);
+                } else trigger_error('Server responded with code ' . $result->info['http_code'], E_USER_ERROR);
+            } else trigger_error('cURL responded with: ' . $result->response[0], E_USER_ERROR);
+        });
+        return $this->result;
     }
 }
